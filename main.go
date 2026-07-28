c9cab9837fa1756fa352ee34bbab557517661089
slikhithreddy22 <slikhithreddy22@gmail.com>
2026-07-28 18:02:04 +0530

    feat : creating a go project

--- main.go
+++ main.go
@@ -1,4 +1,13 @@
 package main
 
+import (
+	"github.com/slikhithreddy22/femProject/internal/app"
+)
+
 func main() {
+	app, err := app.NewApplication()
+	if err != nil {
+		panic(err)
+	}
+	app.Logger.Println("we are running app")
 }
