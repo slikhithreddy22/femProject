5748182667ed0eba7ea53bca6cdb46c6bc7bc99c
slikhithreddy22 <slikhithreddy22@gmail.com>
2026-07-28 18:26:41 +0530

    feat : creating an http server

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	"time"
+
 	"github.com/slikhithreddy22/femProject/internal/app"
 )
 
@@ -10,4 +14,21 @@ func main() {
 		panic(err)
 	}
 	app.Logger.Println("we are running app")
+
+	http.HandleFunc("/healthcheck", HealthCheck)
+	server := &http.Server{
+		Addr:         ":8080",
+		IdleTimeout:  time.Second,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	err = server.ListenAndServe()
+	if err != nil {
+		app.Logger.Fatal(err)
+	}
+}
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Status is available\n")
 }
